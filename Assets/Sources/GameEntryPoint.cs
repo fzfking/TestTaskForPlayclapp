9101dca5fa4d9835f0063ad1ca717aba2292bb2e
fzfking <joker404tv@gmail.com>
2022-10-17 13:08:05 +0300

    Created GameEntryPoint
    Needed for game flow control and throw references from game scene to spawner.

---
+++ Assets/Sources/GameEntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sources.Interfaces;
+using UnityEngine;
+
+namespace Sources
+{
+    public class GameEntryPoint: MonoBehaviour, ICoroutineRunner
+    {
+        [SerializeField] private GameObject SpawnPoint;
+        [SerializeField] private UserSettings Settings;
+        [SerializeField] private CubeModel CubeModelPrefab;
+
+        private Spawner _spawner;
+        private void Start()
+        {
+            _spawner = new Spawner(Settings, this, CubeModelPrefab, SpawnPoint.transform);
+        }
+
+        private void OnDestroy()
+        {
+            _spawner.Destroy();
+        }
+    }
+}
