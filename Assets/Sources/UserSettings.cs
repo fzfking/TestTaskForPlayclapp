fe86507cd1d691a312f6e48bd343c8b1fcbce47a
fzfking <joker404tv@gmail.com>
2022-10-17 13:06:56 +0300

    Created UserSettings
    Combining UI elements to access them

---
+++ Assets/Sources/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sources
+{
+    public class UserSettings : MonoBehaviour
+    {
+        public TMP_InputField DistanceText;
+        public TMP_InputField SpeedText;
+        public TMP_InputField SpawnIntervalText;
+        public Button SpawnToggleButton;
+    }
+}
